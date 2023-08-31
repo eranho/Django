92e7518f343e72c4575098e5edc310709a398292
Eric Oduol <ericoduol@gmail.com>
2023-08-31 12:22:47 +0100

    django 101 completed

---
+++ feed/views.py
@@ -0,0 +1,40 @@
+from django import http
+from django.contrib import messages
+from typing import Any, Dict
+from django.http import HttpResponse
+from django.views.generic import TemplateView, DetailView, FormView
+from .forms import PostForm
+from .models import Post
+
+class HomePageView(TemplateView):
+    template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context ['posts'] = Post.objects.all().order_by('-id')
+        #context ['my_thing'] = "Hello World :p this is dynamic"
+        """context = {
+            'my_thing': 'Hello World from Dudley'
+        }"""
+        return context
+    
+class PostDetailView(DetailView):
+    template_name = "detail.html"
+    model = Post
+
+class AddPostView(FormView):
+    template_name = "new_post.html"
+    form_class = PostForm
+    success_url = "/"
+
+    def dispatch(self, request, *args: Any, **kwargs):
+        self.request = request
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        new_object = Post.objects.create(
+            text = form.cleaned_data['text'],
+            image = form.cleaned_data['image'],
+        )
+        messages.add_message(self.request, messages.SUCCESS, 'Your post was successful')
+        return super().form_valid(form)
